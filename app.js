4ebc0136873e77e562f3a1a72a4ff266c91fe26f
StephenG <publiccomms@tutanota.de>
2020-04-18 16:56:33 +0100

    Added functionality of dynamic type id

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const axios = require('axios');
 const fs = require('fs');
 const dateFormat = require('dateformat');
 
+// 16638
 // fetch data
-(async () => {
-  var data = await axios.get('https://evemarketer.com/api/v1/markets/types/16638?language=en');
+
+async function getData(id){
+  var data = await axios.get(`https://evemarketer.com/api/v1/markets/types/${id}?language=en`);
+
   data = data.data;
 
   // item id, name, price, region, system, station
   let item_sale = {
-    item_id: "",
     name: "",
     volume_entered: "",
     volume_remain: "",
@@ -26,10 +28,9 @@ const dateFormat = require('dateformat');
 
   if(data.sell.length > 0) {
     if(data.sell.length >= 5) {
-      for(let i = 0; i < 6; i++) {
+      for(let i = 0; i < 5; i++) {
         let record = data.sell[i];
         rows.push({
-          "item_id" : item_sale.item_id,
           "name": item_sale.name,
           "volume_entered": record.volume_entered,
           "volume_remain": record.volume_remain,
@@ -39,9 +40,24 @@ const dateFormat = require('dateformat');
         });
       }
     }
+    else {
+      let record = data.sell[0];
+        rows.push({
+          "name": item_sale.name,
+          "volume_entered": record.volume_entered,
+          "volume_remain": record.volume_remain,
+          "price": record.price,
+          "region": record.region.name,
+          "station": record.station.name,
+        });
+    }
   }
+  return rows;
+}
+(async () => {
+  let rows = await getData("16638");
 
-  const fields = ['item_id', 'name', 'volume_entered', 'volume_remain', 'price', 'region', 'station'];
+  const fields = ['name', 'volume_entered', 'volume_remain', 'price', 'region', 'station'];
 
   const json2csvParser = new Parser({ fields });
   let csv = json2csvParser.parse(rows)
